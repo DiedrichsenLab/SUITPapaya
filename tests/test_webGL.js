280ed1a1980df75aaee4657e68707c1eb4be53d7
Da Zhi <dzhi@uwo.ca>
2019-02-28 14:00:33 -0500

    Fixed: mapping the vertices color information with jet colormap; Set threshold to filter the middle range.

--- tests/test_webGL.js
+++ tests/test_webGL.js
@@ -128,16 +128,127 @@ var InitDemo = function () {
     }
     console.log(triangleIndex1);
 
+    // ------------ Load jet-colormap -------------//
+    $.ajax({
+        url: "../tests/data/jet_colormap.csv",
+        async: false,
+        success: function (csvd) {
+            colormap = $.csv.toArrays(csvd);
+        }
+    });
+
+    console.log(colormap);
+
+    // ------------ Load vertices color information -------------//
+    $.ajax({
+        url: "../tests/data/flatmap_verticesColor.csv",
+        async: false,
+        success: function (csvd) {
+            verticesColor = $.csv.toArrays(csvd);
+        }
+    });
+
+    console.log(verticesColor);
+    // var triangleIndex1 = [];
+    //
+    // for (var i = 0; i < edgeData.length; i++) {
+    //     triangleIndex1.push(edgeData[i][0] - 1);
+    //     triangleIndex1.push(edgeData[i][1] - 1);
+    //     triangleIndex1.push(edgeData[i][2] - 1);
+    // }
+    // console.log(triangleIndex1);
+
+    // ------------ Load flatmap edges COLOR information and transfer to array -------------//
+    // var input = [0, 3, 2, 2, 6, 5, NaN];
+    // var colormap = [[0.0, 0.0, 0.5625],
+    //                 [0.0, 0.0, 0.6250],
+    //                 [0.0, 0.0, 0.6875],
+    //                 [0.0, 0.0, 0.7500],
+    //                 [0.0, 0.0, 0.8125]];
+
+    var indices = [];
+    for (var x in verticesColor) {
+        indices.push([verticesColor[x][0], x]);
+    }
+    indices.sort(function (a, b) {
+        if( !isFinite(a[0]) && !isFinite(b[0]) ) {
+            return 0;
+        }
+        if( !isFinite(a[0]) ) {
+            return 1;
+        }
+        if( !isFinite(b[0]) ) {
+            return -1;
+        }
+        return a[0]-b[0];
+        //return a[0] - b[0];
+    });
+
+    var count = 0;
+    for (var i = 0; i < indices.length; i++) {
+        if (isNaN(indices[i][0])) {
+            count += 1;
+            console.log(i);
+        }
+    }
+
+    console.log(count);
+    console.log(indices);
+    console.log(colormap[0][0]);
+
+    var indices_color = [];
+    indices_color.push([indices[0][0], indices[0][1], colormap[0][0], colormap[0][1], colormap[0][2]]);
+    colormap.shift();
+
+    for (var i = 1; i < indices.length; i++) {
+        if (isNaN(indices[i][0])) {
+            indices_color.push([indices[i][0], indices[i][1], 0.9, 0.9, 0.9]);
+        }
+        else{
+            if (indices[i][0] === indices[i-1][0]) {
+                indices_color.push([indices[i][0], indices[i][1], indices_color[i-1][2], indices_color[i-1][3], indices_color[i-1][4]]);
+            }
+            else{
+                indices_color.push([indices[i][0], indices[i][1], colormap[0][0], colormap[0][1], colormap[0][2]]);
+                colormap.shift();
+            }
+        }
+    }
+    console.log(indices_color);
+
+    indices_color.sort(function (a, b) {
+        return a[1] - b[1];
+    });
+    // for (var i = 0; i < indices_color.length; i++){
+    //     console.log(indices_color[i][2] + " " + indices_color[i][3] + " " + indices_color[i][4] + '\n');
+    // }
+    //console.log(indices_color);
+
+    // Set threshold
+    var upper = 1.0;
+    var lower = -0.1;
+
+    for (var i = 0; i < indices_color.length; i++) {
+        if (!isNaN(indices_color[i][0])){
+            if(indices_color[i][0] >= lower && indices_color[i][0] <= upper) {
+                indices_color[i][2] = 0.0;
+                indices_color[i][3] = 0.0;
+                indices_color[i][4] = 0.0;
+            }
+        }
+    }
+
     //var triangleVertices = [0.0, 0.5, -0.5, -0.5, 0.5, -0.5];
     pos = 2;
     interval = 5;
 
     while (pos < triangleVertices1.length) {
-        triangleVertices1.splice(pos, 0, Math.random().toFixed(1), Math.random().toFixed(1), Math.random().toFixed(1));
+        triangleVertices1.splice(pos, 0, indices_color[0][2], indices_color[0][3], indices_color[0][4]);
+        indices_color.shift();
         pos += interval;
     }
 
-    triangleVertices1.push(Math.random().toFixed(1), Math.random().toFixed(1), Math.random().toFixed(1));
+    triangleVertices1.push(indices_color[0][2], indices_color[0][3], indices_color[0][4]);
 
     // triangle vertex buffer object
     const triangleVertexBufferObject = gl.createBuffer();
