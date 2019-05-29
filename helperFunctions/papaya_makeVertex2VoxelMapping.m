b0f36cfc5ea5513607b0a02e2c1cccb8ad77fa3a
Joern Diedrichsen <joern.diedrichsen@googlemail.com>
2019-05-29 18:22:32 -0400

    yz fix

--- helperFunctions/papaya_makeVertex2VoxelMapping.m
+++ helperFunctions/papaya_makeVertex2VoxelMapping.m
@@ -1,9 +1,11 @@
 function papaya_makeVertex2VoxelMapping
-cd('/Users/jdiedrichsen/Matlab/imaging/suit');
-P=gifti('flatmap/PIAL_SUIT.surf.gii'); 
-W=gifti('flatmap/WHITE_SUIT.surf.gii'); 
+P=gifti('/Users/jdiedrichsen/Matlab/imaging/suit/flatmap/PIAL_SUIT.surf.gii'); 
+W=gifti('/Users/jdiedrichsen/Matlab/imaging/suit/flatmap/WHITE_SUIT.surf.gii'); 
 midcoord = (P.vertices + W.vertices)/2; 
-V= spm_vol('templates/SUIT.nii'); 
+V= spm_vol('/Users/jdiedrichsen/Matlab/imaging/suit/templates/SUIT.nii'); 
+% Note that Papaya stores the image in LAS coordinate
 [i,j,k]=spmj_affine_transform(midcoord(:,1),midcoord(:,2),midcoord(:,3),inv(V.mat));  % Transform from x,y,z coordinates to voxels 
-A=[[1:length(i)]' round(i)-1 round(j)-1 round(k)-1]; 
+
+% Since voxels are 0 based and in LAS coordinates
+A=[[1:length(i)]' round(i)-1 V.dim(2)-round(j) V.dim(3)-round(k)]; 
 dlmwrite('Mapping.txt',A,',');
